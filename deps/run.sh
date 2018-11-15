52cf3f514a5f0f6e72e9417ef70aab5215e9657b
Matthew Faltys <mfaltys@gmail.com>
2018-11-15 14:59:13 -0600

    Update image

--- deps/run.sh
+++ deps/run.sh
@@ -2,9 +2,11 @@
 
 # pull images
 # IBM
-wget -O ibm.png https://upload.wikimedia.org/wikipedia/commons/thumb/5/51/IBM_logo.svg/1280px-IBM_logo.svg.png
+#wget -O ibm.png https://upload.wikimedia.org/wikipedia/commons/thumb/5/51/IBM_logo.svg/1280px-IBM_logo.svg.png
+wget -O ibm.jpg https://upload.wikimedia.org/wikipedia/commons/f/fc/IBM_logo_in.jpg
 # RedHat
-wget -O redhat.png https://upload.wikimedia.org/wikipedia/sco/thumb/6/6c/RedHat.svg/1280px-RedHat.svg.png
+#wget -O redhat.png https://upload.wikimedia.org/wikipedia/sco/thumb/6/6c/RedHat.svg/1280px-RedHat.svg.png
+wget -O redhat.jpg http://www.rit.edu/news/lib/filelib/201303/redhat.jpg
 
 # run main script
-../scripts/cpc.sh ibm.png redhat.png
+../scripts/cpc.sh ibm.jpg redhat.jpg
